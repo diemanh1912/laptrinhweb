6f3cbdc0adac648775d073a15605ab6446648203
Your Name <your.email@example.com>
2024-09-10 15:14:03 +0700

    baikiemtra01

---
+++ baikiemtra01/Controllers/SanPhamController.cs
@@ -0,0 +1,16 @@
+﻿using BaiTap06.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        public class SanPhamViewModel
+        {
+            public string TenSanPham { get; set; }
+            public decimal GiaBan { get; set; }
+            public string AnhMoTa { get; set; }
+        }
+    }
+
+}
