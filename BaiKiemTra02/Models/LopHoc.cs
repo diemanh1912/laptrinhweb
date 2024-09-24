254ac50b516d06d92dc536090067d823ab05cdf8
Your Name <nguyenhongdiemanh@gmail.com>
2024-09-24 16:24:59 +0700

    baikiemtra02

---
+++ BaiKiemTra02/Models/LopHoc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BaiKiemTra02.Models
+{
+    public class LopHoc
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string TenLopHoc { get; set; }
+
+        [Required]
+        public int NamNhapHoc { get; set; } // Thay đổi kiểu dữ liệu thành int
+
+        [Required]
+        public int NamRaTruong { get; set; } // Thay đổi kiểu dữ liệu thành int
+
+        [Required]
+        public int SoLuongSinhVien { get; set; }
+    }
+}
